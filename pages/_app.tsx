5475bcd365b6b17e541bfee0201b346a9efbc4bd
Leandro Atallah <leandroatallah@gmail.com>
2021-07-24 03:23:36 -0300

    Add global Styles Components styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,60 @@
 import React from "react"
-import "../styles/globals.css"
 import type { AppProps } from "next/app"
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    vertical-align: baseline;
+  }
+
+  article,
+  aside,
+  details,
+  figcaption,
+  figure,
+  footer,
+  header,
+  hgroup,
+  menu,
+  nav,
+  section {
+    display: block;
+  }
+  body {
+    line-height: 1;
+    background-color: #fff;
+    font-family: 'Roboto', sans-serif;
+  }
+  ol,
+  ul {
+    list-style: none;
+  }
+  blockquote,
+  q {
+    quotes: none;
+  }
+  blockquote:before,
+  blockquote:after,
+  q:before,
+  q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+`
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }
 export default MyApp
